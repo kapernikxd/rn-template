f953ae63205c26406b67f88a1fbbff6e6e8af0db
kapernikxd <kapernikxd@gmail.com>
2025-10-25 01:36:49 +0200

    change domain

--- src/constants/links.ts
+++ src/constants/links.ts
@@ -4,17 +4,17 @@ export const isDev = process.env.NODE_ENV === "development";
 
 export const BASE_URL = isDev
   ? "http://192.168.0.20:5001/"
-  : "https://pllace.su/";
+  : "https://aipair.pro/";
   
 
 export const TELEGRAM_URL = 'https://t.me/pllacesupport';
-export const DOMAIN = "https://pllace.su"
+export const DOMAIN = "https://AiPair.pro"
 export const API_URL = `${BASE_URL}api/`;
 
 
-export const EMAIL = "pllace.support@gmail.com";
+export const EMAIL = "AiPairPro@yandex.ru";
 
-export const SITE_NAME = "Pllace";
+export const SITE_NAME = "AiPair";
 
 
 export const appVersion = Constants.expoConfig?.version || '1.0.0';
