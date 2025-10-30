80e7c57b28bc3ef32c70224322e460beadc4cd24
kapernikxd <kapernikxd@gmail.com>
2025-10-30 21:32:57 +0100

    add onboarding

--- src/helpers/utils/onboarding.ts
+++ src/helpers/utils/onboarding.ts
@@ -10,7 +10,7 @@ export const ONBOARDING_PHOTO_3 = Image.resolveAssetSource(
   require(`../../assets/onboarding/onboarding3.png`)
 ).uri;
 export const ONBOARDING_PHOTO_4 = Image.resolveAssetSource(
-  require(`../../assets/onboarding/onboarding4.png`)
+  require(`../../assets/onboarding/onboarding4.gif`)
 ).uri;
 export const ONBOARDING_PHOTO_5 = Image.resolveAssetSource(
   require(`../../assets/onboarding/onboarding5.png`)
