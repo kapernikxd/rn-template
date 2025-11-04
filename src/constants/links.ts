b4571a0dd6780092b46562769686a50d1bf743df
kapernikxd <kapernikxd@gmail.com>
2025-11-04 17:53:03 +0100

    fix

--- src/constants/links.ts
+++ src/constants/links.ts
@@ -26,10 +26,11 @@ export const appVersion = Constants.expoConfig?.version || '1.0.0';
 
 export const GOOGLE_SIGN_IN_CLIENT_ID = '456854793341-f96jb6ks2q6pm18q9b5tmj869j4rb5mg.apps.googleusercontent.com' // из Google Cloud Console web
 
-export const ANDROID_AD_UNIT_ID_BANNER = 'ca-app-pub-8636022279548301/8567360540';
-export const ANDROID_AD_UNIT_ID_REWARD = 'ca-app-pub-8636022279548301/9133642229';
+export const ANDROID_AD_UNIT_ID_BANNER = 'ca-app-pub-8636022279548301/4326455527';
+export const ANDROID_AD_UNIT_ID_REWARD = 'ca-app-pub-8636022279548301~3808522726';
 
-export const IOS_AD_UNIT_ID_BANNER = 'ca-app-pub-8636022279548301/7481186180';
-export const IOS_AD_UNIT_ID_REWARD = 'ca-app-pub-8636022279548301/2111092392';
+export const IOS_AD_UNIT_ID_BANNER = 'ca-app-pub-8636022279548301/6952618865';
+export const IOS_AD_UNIT_ID_REWARD = 'ca-app-pub-8636022279548301/2882453863';
 
 export const TOKEN_REWARD_AMOUNT = 10;
+export const DEFAULT_TOKEN_BALANCE = 20;
