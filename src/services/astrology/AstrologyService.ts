1392ca1bdb3d43bd323f9eee3214a4ca16dee475
kapernikxd <54470526+kapernikxd@users.noreply.github.com>
2025-12-02 23:01:53 +0100

    Add language parameter to horoscope generation

--- src/services/astrology/AstrologyService.ts
+++ src/services/astrology/AstrologyService.ts
@@ -1,17 +1,29 @@
 import type { AxiosResponse } from 'axios';
 
+import { LANGUAGE_OPTIONS, normalizeLanguageCode } from '../../constants/languages';
+import i18n from '../../helpers/i18n';
 import $api from '../../helpers/http';
 import type { ProfileInfoData } from '../../helpers/profile/profileInfoStorage';
 import type { HoroscopeCategory, HoroscopeResponse } from '../../types/astrology';
 
 class AstrologyService {
+  private getLanguageParam(languageCode?: string): string {
+    return (
+      normalizeLanguageCode(languageCode ?? i18n.language ?? i18n.resolvedLanguage) ??
+      LANGUAGE_OPTIONS[0].code
+    );
+  }
+
   async generateHoroscope(
     category: HoroscopeCategory,
     profile: ProfileInfoData,
+    languageCode?: string,
   ): Promise<HoroscopeResponse> {
+    const lang = this.getLanguageParam(languageCode);
     const response: AxiosResponse<HoroscopeResponse> = await $api.post(
       `astrology/${category}`,
       profile,
+      { params: { lang } },
     );
 
     return response.data;
