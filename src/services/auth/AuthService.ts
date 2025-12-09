c1a21062d46a8dfa40fa80889efdaf66a3b2fa43
kapernikxd <54470526+kapernikxd@users.noreply.github.com>
2025-12-09 17:57:35 +0100

    Add app name when sending push token

--- src/services/auth/AuthService.ts
+++ src/services/auth/AuthService.ts
@@ -2,7 +2,7 @@
 import axios, { AxiosResponse, isAxiosError } from "axios";
 import $api from "../../helpers/http";
 import { AuthRejection, AuthResponse, AuthResponseExtend, LoginParams, NewPasswordParams, ParamsVerificateEmail, RegistrationParams, ResetPasswordParams } from "../../types/auth";
-import { API_URL } from "../../constants/links";
+import { API_URL, SITE_NAME } from "../../constants/links";
 
 export default class AuthService {
   static async login({
@@ -102,6 +102,6 @@ export default class AuthService {
   }
 
   static async sendPushToken(pushToken: string): Promise<void> {
-    return $api.post("/push/mobile", { pushToken });
+    return $api.post("/push/mobile", { pushToken, appName: SITE_NAME });
   }
 }
